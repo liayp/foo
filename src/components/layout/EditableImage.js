f3edcf63a7dcd274eb11143d7caec8fcbbfcb7d5
liayp <amaliaypanane@gmail.com>
2023-12-21 19:18:48 +0800

    try  to fix

--- src/components/layout/EditableImage.js
+++ src/components/layout/EditableImage.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import toast from "react-hot-toast";
 
-export default function EditableImage({link, setLink}) {
+export default function EditableImage({ link, setLink }) {
 
   async function handleFileChange(ev) {
     const files = ev.target.files;
